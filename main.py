cc7b06552684f4858088552f3716de3ae70b4bcd
Victor Mota <victor.vmt@outlook.com>
2020-05-20 17:23:24 -0300

    Iniciando projeto

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('./data/aluguel.csv', sep=';')
+
+tipos_imoveis = pd.DataFrame(dataset['Tipo'].drop_duplicates())
+tipos_imoveis.index = range(tipos_imoveis.shape[0])  # refazendo índice
+tipos_imoveis.columns.name = 'id'  # renomeando coluna
+
+tipos_residenciais = ['Quitinete', 'Casa', 'Apartamento', 'Casa de Condomínio', 'Casa de Vila']
+
+# Filtrando resultados
+dados_residenciais = dataset[dataset['Tipo'].isin(tipos_residenciais)]
+
+print(len(dataset))
+print(len(dataset['Tipo'].isin(tipos_residenciais)))
