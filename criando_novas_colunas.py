b3c2bb57608bc252cd8f4fe6105e43a1b1683244
Victor Mota <victor.vmt@outlook.com>
2020-05-21 18:35:05 -0300

    Adicionando exemplos de remoções de colunas

--- criando_novas_colunas.py
+++ criando_novas_colunas.py
@@ -13,3 +13,18 @@ casas = ['Casa', 'Casa de Condomínio', 'Casa de Vila']
 dados['Tipo Agregado'] = dados['Tipo'].apply(lambda value: 'Casa' if value in casas else 'Aparamento')
 
 print(dados[['Tipo Agregado', 'Valor', 'Valor Bruto', 'Valor m2', 'Valor Bruto m2']].head())
+
+print('\nRemovendo colunas:\n')
+dados_aux = dados[['Tipo Agregado', 'Valor', 'Valor Bruto', 'Valor m2', 'Valor Bruto m2']]
+
+del dados_aux['Valor Bruto']
+dados_aux.pop('Valor Bruto m2')
+
+print(dados_aux.head())
+
+print('\nRemovendo do dataframe original\n')
+dados.drop(['Valor Bruto', 'Valor Bruto m2'], axis=1, inplace=True)
+
+print(dados.head())
+
+dados.to_csv('./data/aluguel_residencial_sem_valor_bruto.csv', sep=';', index=False)
